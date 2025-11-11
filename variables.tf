58bdf9e6a9fb61b849e0b676af593d703f048b24
Arno Moelich <arno.moelich@atu.ie>
2025-11-11 14:07:29 +0000

    Populate provider.tf and variables.tf for initialization process

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+    description = "Name of resource grioup"
+    type = string
+    default = "Terraform_Lab2"
+}
+
+variable "location" {
+    description = "Azure region where reources will be created and hosted"
+    type = string
+    default = "westeurope"
+}

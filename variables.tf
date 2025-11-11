656b2c2382e4273fcaaa17e6fa026cd02c4d2afa
Arno Moelich <arno.moelich@atu.ie>
2025-11-11 14:15:01 +0000

    use terraform.fmt for linting/formatting

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group_name" {
-    description = "Name of resource grioup"
-    type = string
-    default = "Terraform_Lab2"
+  description = "Name of resource grioup"
+  type        = string
+  default     = "Terraform_Lab2"
 }
 
 variable "location" {
-    description = "Azure region where reources will be created and hosted"
-    type = string
-    default = "westeurope"
+  description = "Azure region where reources will be created and hosted"
+  type        = string
+  default     = "westeurope"
 }

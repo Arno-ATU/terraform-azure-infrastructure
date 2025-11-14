e5039d72a710ea682d49b58bade87d700a44920e
Arno Moelich <arno.moelich@atu.ie>
2025-11-14 15:23:58 +0000

    Add outputs for subnets IDs and CIDR blocks
    - Outputs for all 4 subnet IDs
    - CIDR block outputs for public subnets
    - Enables easyy reference to subnet information

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,38 @@ output "vnet_id" {
   description = "ID of the created virtual network"
   value       = azurerm_virtual_network.main.id
 }
+
+
+# ===========================================================
+# SUBNET OUTPUTS
+# ===========================================================
+
+output "public_subnet_1_id" {
+  description = "ID of public subnet in Availability Zone 1"
+  value       = azurerm_subnet.public_1.id
+}
+
+output "public_subnet_1_cidr" {
+  description = "CIDR block of public subnet 1"
+  value       = azurerm_subnet.public_1.address_prefixes[0]
+}
+
+output "public_subnet_2_id" {
+  description = "ID of public subnet in Availability Zone 2"
+  value       = azurerm_subnet.public_2.id
+}
+
+output "public_subnet_2_cidr" {
+  description = "CIDR block of public subnet 2"
+  value       = azurerm_subnet.public_2.address_prefixes[0]
+}
+
+output "private_subnet_1_id" {
+  description = "ID of private subnet in Availability Zone 1"
+  value       = azurerm_subnet.private_1.id
+}
+
+output "private_subnet_2_id" {
+  description = "ID of private subnet in Availability Zone 2"
+  value       = azurerm_subnet.private_2.id
+}

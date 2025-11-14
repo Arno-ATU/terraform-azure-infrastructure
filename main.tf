bcc3d2bf656eac3d807d7aee8b107bc44ee4b1f4
Arno Moelich <arno.moelich@atu.ie>
2025-11-14 14:15:58 +0000

    Add ppublic subnets for web tier across two availability zones
    - Created a subnet-public-az1 and -az2 for zones 1 and 2
    - Public subnets will host web servers and load balancers
    - 256 useable IP addresses

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
+# Resource Group - Container for all resources
 
-# Resource Group - Container for all our resources
-# Azure requires all resources to belong to a resource group
 
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
@@ -14,10 +13,8 @@ resource "azurerm_resource_group" "main" {
   }
 }
 
-# Virtual Network - Its our private network in Azure
-# Why: VNet provides isolated network space with CIDR 10.0.0.0/16
-# This gives us 65,536 IP addresses to work with
 
+# Virtual Network
 resource "azurerm_virtual_network" "main" {
   name                = "vnet-ha-webapp"
   location            = azurerm_resource_group.main.location
@@ -29,4 +26,27 @@ resource "azurerm_virtual_network" "main" {
     Project     = "Terraform-Assignment"
     Student     = var.student_name
   }
+}
+
+
+# Public Subnet 1 - Availability Zone 1
+# Zone 1 provides isolation from Zone 2 for high availability
+
+resource "azurerm_subnet" "public_1" {
+  name                 = "subnet-public-az1"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+
+# Public Subnet 2 - Availability Zone 2
+# If Zone 1 fails, Zone 2 continues serving traffic
+# Provides 256 IP addresses (10.0.2.1 - 10.0.2.254)
+
+resource "azurerm_subnet" "public_2" {
+  name                 = "subnet-public-az2"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
